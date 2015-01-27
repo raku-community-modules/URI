4bbb8e7e39be0ec98c236e951f736a801ab2305f
Nick Logan <ugexe@cpan.org>
2015-01-27 17:03:56 -0500

    Add default ports for: smtp,shttp,sftp,nntp,git,imap

--- lib/URI/DefaultPort.pm
+++ lib/URI/DefaultPort.pm
@@ -7,13 +7,18 @@ package URI::DefaultPort {
 
     my %default_port = (
         ftp     =>      21,
+        sftp    =>      22,
         ssh     =>      22,
         telnet  =>      23,
         tn3270  =>      23,
+        smtp    =>      25,
         gopher  =>      70,
         http    =>      80,
+        shttp   =>      80,
         pop     =>      110,
         news    =>      119,
+        nntp    =>      119,
+        imap    =>      143,
         ldap    =>      389,
         https   =>      443,
         rlogin  =>      513,
@@ -24,7 +29,8 @@ package URI::DefaultPort {
         rsync   =>      873,
         mms     =>      1755,
         sip     =>      5060,
-        sips    =>      5061
+        sips    =>      5061,
+        git     =>      9418
     );
     
     our sub scheme_port(Str $scheme) {
