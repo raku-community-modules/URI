b56ff3787bb7a0958fafcea3bca5db916861d00e
Ronald Schmidt <ronaldxs@software-path.com>
2015-05-19 17:45:31 -0400

    Created named regexes of escape character sets and simplified coding

--- lib/URI/Escape.pm
+++ lib/URI/Escape.pm
@@ -1,51 +1,53 @@
 use v6;
 
-package URI::Escape {
+module URI::Escape;
 
-    use IETF::RFC_Grammar::URI;
+use IETF::RFC_Grammar::URI;
 
-    my %escapes = (^256).for: {
-        ;
-        .chr => sprintf '%%%02X', $_
-    };
+my %escapes = (^256).for: {
+    .chr => sprintf '%%%02X', $_
+};
 
-    # in moving from RFC 2396 to RFC 3986 this selection of characters
-    # may be due for an update ...
-
-    # commented line below used to work ...
-#    token artifact_unreserved {<[!*'()] +IETF::RFC_Grammar::URI::unreserved>};
-
-    sub uri_escape($s, Bool :$no_utf8 = False) is export {
-        return $s unless defined $s;
-        $s.subst(:g, rx/<- [!*'()\-._~A..Za..z0..9]>/,
-            {
-                ( $no_utf8 || .Str.ord < 128 ) ?? %escapes{ .Str } !!
-                    # as unpack progresses the line below can improve ...
-                    '%' ~ .Str.encode.unpack('H*').uc.comb(/../).join('%')
-            }
-        );
+# in moving from RFC 2396 to RFC 3986 this selection of characters
+# may be due for an update ...
+grammar escape-charset is IETF::RFC_Grammar::URI {
+    token escape_unreserved {
+        <[!*'()] +unreserved>
     }
+    token escape_chars {
+        <- escape_unreserved>
+    }
+}
 
-    # todo - automatic invalid UTF-8 detection
-    # see http://www.w3.org/International/questions/qa-forms-utf-8
-    #     find first sequence of %[89ABCDEF]<.xdigit>
-    #         use algorithm from url to determine if it's valid UTF-8
-    sub uri_unescape(*@to_unesc, Bool :$no_utf8 = False) is export {
-        my @rc = @to_unesc.for: {
-            .trans('+' => ' ')\
-            .subst(:g, / [ '%' (<.xdigit> ** 2 ) ]+ /, -> $/ {
-                $no_utf8
-                    ?? join('', $0.for({ :16(~$_).chr }))
-                    !! Buf.new($0.for({ :16(~$_) })).decode('UTF-8')
-            })
-        };
-
-        return do given @rc.elems { # this might be simplified some day
-            when 0 { Nil }
-            when 1 { @rc[0] }
-            default { @rc }
+sub uri_escape($s, Bool :$no_utf8 = False) is export {
+    return $s unless defined $s;
+    $s.subst(:g, /<escape-charset::escape_chars>/,
+        {
+            ( $no_utf8 || .Str.ord < 128 ) ?? %escapes{ .Str } !!
+                # as unpack progresses the line below can improve ...
+                .Str.encode.unpack('H*').uc.subst(:g, /../, { '%' ~ $_ })
         }
+    );
+}
+
+# todo - automatic invalid UTF-8 detection
+# see http://www.w3.org/International/questions/qa-forms-utf-8
+#     find first sequence of %[89ABCDEF]<.xdigit>
+#         use algorithm from url to determine if it's valid UTF-8
+sub uri_unescape(*@to_unesc, Bool :$no_utf8 = False) is export {
+    my @rc = @to_unesc.for: {
+        .trans('+' => ' ')\
+        .subst(:g, / [ '%' (<.xdigit> ** 2 ) ]+ /,
+            -> ( $escaped_octets ) {
+                $no_utf8
+                    ?? ($escaped_octets.map: { :16(~$_).chr }).join
+                    !! Buf.new($escaped_octets.map: { :16(~$_) }).\
+                        decode('UTF-8')
+            }
+        )
     }
+
+    @rc.elems > 1 ?? @rc !! @rc[0];
 }
 
 =begin pod
