66726e7ed7010ccc52f3c6832173a3f15dcb7230
Ronald Schmidt <ronaldxs@software-path.com>
2015-05-19 17:44:01 -0400

    Added POD documentation of new interface

--- lib/URI/DefaultPort.pm
+++ lib/URI/DefaultPort.pm
@@ -3,6 +3,19 @@ use v6;
 # This logic seems to belong somewhere related to URI but not in the URI
 # module itself.
 
+=begin pod
+
+=head1 URI::DefaultPort
+
+C<URI::DefaultPort> Default ports for URI schemes.
+
+=head1 Synopsis
+
+    use URI::DefaultPort
+    my $port = scheme_default_port('ftp');
+
+=end pod
+
 use IETF::RFC_Grammar::URI;
 
 module URI::DefaultPort;
