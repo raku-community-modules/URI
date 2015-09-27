52ae921103337b467b43e39347cd3b018e88451d
Ronald Schmidt <ronaldxs@software-path.com>
2015-09-27 11:50:06 -0400

    finished moving all rules to kebab case

--- lib/IETF/RFC_Grammar/URI.pm
+++ lib/IETF/RFC_Grammar/URI.pm
@@ -3,17 +3,8 @@ use v6;
 # Taken/Copied with relatively minor translation to Perl6
 # from RFC 3986 (http://www.ietf.org/rfc/rfc3986.txt)
 
-#`{{
-
-There was a bug with combined character classes and kebab
-character class names that was reported on irc and fixed on 9/18/2015.
-Most rules have been translated to kebab case conformant to the
-RFC but character classes had to be left with underscores for now.
-The latest rakudos have a fix and sometime soon that will be published
-as part of a rakudo star release and then I can kebab case the character
-classes in this grammar.
-
-}}
+# Rule names moved from snake case with _ to kebab case with -
+# 9/2015.  The rfc grammar is specified in kebab case.
 
 use IETF::RFC_Grammar::IPv6;
 
@@ -40,7 +31,7 @@ token relative-ref-non-empty      {
     <relative-part-non-empty> [ '?' <query> ]? [ '#' <fragment> ]?
 };
 token relative-part-non-empty     {
-    | <?[/]> '//' <authority> <path_abempty>
+    | <?[/]> '//' <authority> <path-abempty>
     | <path-absolute>
     | <path-noscheme>
 };
@@ -56,7 +47,7 @@ token hier-part     {
     | <path-empty>
 };
 
-token scheme            { <.uri_alpha> <[\-+.] +uri_alpha +digit>* };
+token scheme            { <.uri-alpha> <[\-+.] +uri-alpha +digit>* };
     
 token authority         { [ <userinfo> '@' ]? <host> [ ':' <port> ]? };
 token userinfo          {
@@ -64,16 +55,16 @@ token userinfo          {
 };
 # the rfc refers to username:password as deprecated
 token likely-userinfo-component {
-    <+unreserved +sub_delims>+ | <.pct-encoded>+
+    <+unreserved +sub-delims>+ | <.pct-encoded>+
 };
 token host              { <IPv4address> | <IP-literal> | <reg-name> };
 token port              { <.digit>* };
 
 token IP-literal        { '[' [ <IPv6address> | <IPvFuture> ] ']' };
 token IPvFuture         {
-    'v' <.xdigit>+ '.' <[:] +unreserved +sub_delims>+
+    'v' <.xdigit>+ '.' <[:] +unreserved +sub-delims>+
 };
-token reg-name          { [ <+unreserved +sub_delims> | <.pct-encoded> ]* };
+token reg-name          { [ <+unreserved +sub-delims> | <.pct-encoded> ]* };
 
 token path-abempty      { [ '/' <segment> ]* };
 token path-absolute     { '/' [ <segment-nz> [ '/' <segment> ]* ]? };
@@ -83,24 +74,24 @@ token path-empty        { <.pchar> ** 0 }; # yes - zero characters
 
 token   segment         { <.pchar>* };
 token   segment-nz      { <.pchar>+ };
-token   segment-nz-nc   { [ <+unenc_pchar - [:]> | <.pct-encoded> ] + };
+token   segment-nz-nc   { [ <+unenc-pchar - [:]> | <.pct-encoded> ] + };
 
 token query             { <.fragment> };
-token fragment          { [ <[/?] +unenc_pchar> | <.pct-encoded> ]* };
+token fragment          { [ <[/?] +unenc-pchar> | <.pct-encoded> ]* };
 
-token pchar             { <.unenc_pchar> | <.pct-encoded> };
-token unenc_pchar       { <[:@] +unreserved +sub_delims> };
+token pchar             { <.unenc-pchar> | <.pct-encoded> };
+token unenc-pchar       { <[:@] +unreserved +sub-delims> };
 
 token pct-encoded       { '%' <.xdigit> <.xdigit> };
 
-token unreserved        { <[\-._~] +uri_alphanum> };
+token unreserved        { <[\-._~] +uri-alphanum> };
 
-token reserved          { <+gen_delims +sub_delims> };
+token reserved          { <+gen-delims +sub-delims> };
 
-token gen_delims        { <[:/?\#\[\]@]> };
-token sub_delims        { <[;!$&'()*+,=]> };
+token gen-delims        { <[:/?\#\[\]@]> };
+token sub-delims        { <[;!$&'()*+,=]> };
 
-token uri_alphanum      { <+uri_alpha +digit> };   
-token uri_alpha         { <[A..Za..z]> };
+token uri-alphanum      { <+uri-alpha +digit> };   
+token uri-alpha         { <[A..Za..z]> };
 
 # vim:ft=perl6
