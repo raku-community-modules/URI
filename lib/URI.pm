5cdd640419fc81ea85bfcfac74c6a7ce1140459e
Jonathan Stowe <jns@gellyfish.co.uk>
2021-02-22 17:57:39 +0000

    Remove the meta test as Test::META depends on URI
    Fixes #46

--- lib/URI.pm
+++ lib/URI.pm
@@ -1,4 +1,4 @@
-unit class URI:auth<github:perl6-community-modules>:ver<v0.3.0>;
+unit class URI:auth<github:perl6-community-modules>:ver<v0.3.1>;
 
 use IETF::RFC_Grammar;
 use IETF::RFC_Grammar::URI;
